9758e60c726827bdc888e6868e114309e8560ff2
Kevin Bell <bellKevin@pm.me>
2022-10-07 13:43:23 -0600

    Update ch6LowestScoreDrop.cpp

--- ch6LowestScoreDrop/ch6LowestScoreDrop/ch6LowestScoreDrop.cpp
+++ ch6LowestScoreDrop/ch6LowestScoreDrop/ch6LowestScoreDrop.cpp
@@ -9,30 +9,30 @@ int findLowest();
 double score, lowestScore = 1000, average = 0;
 int main()
 {
-    cout << "Ch 6 Lowest Score Drop by Kevin Bell\n\n";
-    string response = "y";
+	cout << "Ch 6 Lowest Score Drop by Kevin Bell\n\n";
+	string response = "y";
 	while (response == "y" || response == "Y" || response == "Yes" || response == "YES" || response == "yes") {
-        cout << "Enter five test scores. I will drop the lowest one and average the other four.\n\n";
-        for (int i = 0; i < 5; i++) {
+		cout << "Enter five test scores. I will drop the lowest one and average the other four.\n\n";
+		for (int i = 0; i < 5; i++) {
 			getScore();
-        } // end for loop
+		} // end for loop
 		lowestScore = findLowest();
-        cout << "Lowest score being dropped: " << lowestScore << endl;
+		cout << "Lowest score being dropped: " << lowestScore << endl;
 		calcAverage();
-        cout << "After dropping lowest score, test average is " << average << endl << endl;
-        cout << "Do you want to enter more scores? (y/n) ";
-        cin >> response;
+		cout << "After dropping lowest score, test average is " << average << endl << endl;
+		cout << "Do you want to enter more scores? (y/n) ";
+		cin >> response;
 	} // end while loop
-    //Keep window open til done
-    system("pause");
-    return 0;
+	//Keep window open til done
+	system("pause");
+	return 0;
 } // end main
 
 void getScore() {
 	double score;
 	cout << "Enter a test score between 0 and 100: ";
 	cin >> score;
-	if (score < 0 || score > 100) {
+	while (score < 0 || score > 100) {
 		cout << "Error. Enter a test score between 0 and 100: ";
 		cin >> score;
 	} // end if condition
