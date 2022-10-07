c87b5cd793986fcbd59e08a360241b99703e00b7
Kevin Bell <bellKevin@pm.me>
2022-10-07 12:42:17 -0600

    Update ch6LowestScoreDrop.cpp

--- ch6LowestScoreDrop/ch6LowestScoreDrop/ch6LowestScoreDrop.cpp
+++ ch6LowestScoreDrop/ch6LowestScoreDrop/ch6LowestScoreDrop.cpp
@@ -3,23 +3,22 @@
 
 #include <iostream>
 using namespace std;
-
+void getScore();
+void calcAverage();
+int findLowest();
+double score, lowestScore = 1000, average = 0;
 int main()
 {
     cout << "Ch 6 Lowest Score Drop by Kevin Bell\n\n";
     string response = "y";
-    double score, lowestScore=1000, average=0;
-    while (response == "y") {
+	while (response == "y" || response == "Y" || response == "Yes" || response == "YES" || response == "yes") {
         cout << "Enter five test scores. I will drop the lowest one and average the other four.\n\n";
-        for (int i = 0; i < 5; i++) {
-            cout << "Enter a test score between 0 and 100: ";
-            cin >> score;
-            if (score < 0 || score>100) {
-                cout << "Error. Enter a test score between 0 and 100: ";
-                cin >> score;
-            } // end if condition
+        for (int i = 0; i < 6; i++) {
+			getScore();
         } // end for loop
+		lowestScore = findLowest();
         cout << "Lowest score being dropped: " << lowestScore << endl;
+		calcAverage();
         cout << "After dropping lowest score, test average is " << average << endl << endl;
         cout << "Do you want to enter more scores? (y/n) ";
         cin >> response;
@@ -28,3 +27,24 @@ int main()
     system("pause");
     return 0;
 } // end main
+
+void getScore() {
+	double score;
+	cout << "Enter a test score between 0 and 100: ";
+	cin >> score;
+	if (score < 0 || score>100) {
+		cout << "Error. Enter a test score between 0 and 100: ";
+		cin >> score;
+	} // end if condition
+} // end getScore
+
+void calcAverage() {
+	average = (score + score + score + score+score) / 5;
+} // end calcAverage
+
+int findLowest() {
+	if (score < lowestScore) {
+		lowestScore = score;
+	} // end if condition
+	return lowestScore;
+} // end findLowest
